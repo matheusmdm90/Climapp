94b6b5612c4a85d047cdbfc075ea2e73e40c792a
matheusmdm90 <mdm.matheusswat@gmail.com>
2026-02-21 07:01:34 -0300

    fix comit

---
+++ app/index.tsx
@@ -0,0 +1,51 @@
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function Index() {
+  return (
+    <LinearGradient colors={["#00457d", "#05051f"]} style={styles.container}>
+      <Image source={require("../assets/images/Logo.png")} />
+      <Image source={require("../assets/images/Ilustra.png")} />
+      <Text style={styles.title}>Boas Vindas!</Text>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.textbtn}>Entrar</Text>
+        <MaterialIcons name="arrow-forward" size={24} color="#01080E" />
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 64,
+    paddingVertical: 79,
+    paddingHorizontal: 32,
+  },
+
+  title: {
+    fontSize: 25,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+
+  btn: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#7693FF",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 32,
+    flexDirection: "row",
+    gap: 8,
+  },
+
+  textbtn: {
+    color: "#01080E",
+    fontSize: 20,
+    fontWeight: "600",
+  },
+});
